772a48cdf5d007f3c1149bf44f39a34a5945a8fe
Danchik452 <fisherd2004@gmail.com>
2023-03-13 09:28:21 +0700

    comments

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import ru_local as ru
 print(ru.TASK)
 print(ru.INPUT)
 
-dst_miles = 16637000000  # distance from sond to sun in miles
-spd_sond = 38241  # sond speed in miles per hour
-spd_waves = 299792458 * 2.24  # speed of radio waves in miles per hour
+# distance from sond to sun in miles
+dst_miles = 16_637_000_000
 
-days = int(input())  # number of days since September 25, 2009
+# sond speed in miles per hour
+spd_sond = 38241
+
+# speed of radio waves in miles per hour
+spd_waves = 299_792_458 * 2.24
+
+# number of days since September 25, 2009
+days = int(input())
 
 dst_miles += days * 24 * spd_sond
-dst_km = dst_miles * 1.61  # distance from sond to sun in kilometers
-dst_au = dst_km / 149600000  # distance from sond to sun in astoronomical units
 
-delay = dst_miles / spd_waves  # wireless delay in hours
+# distance from sond to sun in kilometers
+dst_km = dst_miles * 1.61
+
+# distance from sond to sun in astoronomical units
+dst_au = dst_km / 149600000
+
+# wireless delay in hours
+delay = dst_miles / spd_waves
 
 
 print(ru.DST_MILES, dst_miles)
